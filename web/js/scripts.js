64c7ed70368c56b3b5ed0064c1cdb253aeeeda36
Hussain Abbas <hussain@hussainabbas.com>
2016-03-05 16:31:08 -0500

    gallery + contact sections

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -4,6 +4,7 @@
     hamburger();
     navAlt();
     navHide();
+    contactForm();
     $(window).scroll(function() {
       navAlt();
       navHide();
@@ -47,4 +48,25 @@ var navHide = function() {
   } else {
     $('nav').removeClass('hide');
   }
+}
+
+var contactForm = function () {
+  $("#contact-submit").on('click', function () {
+    $contact_form = $('#contact-form');
+    var fields = $contact_form.serialize();
+    $.ajax({
+      type: "POST",
+      url: "js/contact.php",
+      data: fields,
+      dataType: 'json',
+      success: function (response) {
+        if (response.status) {
+          $('#contact-form input').val('');
+          $('#contact-form textarea').val('');
+        }
+        $('#response').empty().html(response.html);
+      }
+    });
+    return false;
+  });
 }
