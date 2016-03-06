323a99b147988c705ff26a99e28183c4ac926c34
Hussain Abbas <hussain@hussainabbas.com>
2016-03-06 00:23:00 -0500

    web about section

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -5,6 +5,7 @@
     navAlt();
     navHide();
     contactForm();
+    accordian();
     $(window).scroll(function() {
       navAlt();
       navHide();
@@ -72,5 +73,22 @@ var contactForm = function () {
       }
     });
     return false;
+  });
+}
+
+var accordian = function () {
+  var accordion_trigger = $('.accordion-heading.accordionize');
+
+  accordion_trigger.delegate('.accordion-toggle', 'click', function (event) {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+      $(this).addClass('inactive');
+    } else {
+      accordion_trigger.find('.active').addClass('inactive');
+      accordion_trigger.find('.active').removeClass('active');
+      $(this).removeClass('inactive');
+      $(this).addClass('active');
+    }
+    event.preventDefault();
   });
 }
